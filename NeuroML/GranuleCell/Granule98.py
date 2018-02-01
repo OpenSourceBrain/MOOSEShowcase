7dcfcb645e390d4dafc1a6c3b7aa4416f88e9732
Dilawar Singh <dilawars@ncbs.res.in>
2018-02-01 19:45:59 +0530

    Fixes to error same as BhallaLab/moose-core#186.

--- NeuroML/GranuleCell/Granule98.py
+++ NeuroML/GranuleCell/Granule98.py
@@ -23,7 +23,7 @@ def loadGran98NeuroML_L123(filename, nogui=False):
     soma_path = populationDict['Gran'][1][0].path+'/Soma_0'
     somaVm = setupTable('somaVm',moose.Compartment(soma_path),'Vm')
     somaCa = setupTable('somaCa',moose.CaConc(soma_path+'/Gran_CaPool_98'),'Ca')
-    somaIKCa = setupTable('somaIKCa',moose.HHChannel(soma_path+'/Gran_KCa_98'),'Gk')
+    somaIKCa = setupTable('somaIKCa',moose.element(soma_path+'/Gran_KCa_98'),'Gk')
 
     soma = moose.Compartment(soma_path)
     print("Reinit MOOSE ... ")
